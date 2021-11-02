dbf3ccf104634e7ae754070f0e940bf17353ef95
zentan2018 <54927578+zentan2018@users.noreply.github.com>
2021-11-02 12:08:10 +0800

    Section 2 - Update of create_tables script

--- Section2/sql/create_tables.sql
+++ Section2/sql/create_tables.sql
@@ -1,25 +1,45 @@
+CREATE DATABASE car_dealership;
+\c car_dealership;
+
+-- Creation of salesperson table
+CREATE TABLE IF NOT EXISTS salesPerson (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR NOT NULL
+);
+
+-- Creation of manufacture table
+CREATE TABLE IF NOT EXISTS manufacture (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR NOT NULL
+);
+
 -- Creation of car table
 CREATE TABLE IF NOT EXISTS car (
-  car_id INT NOT NULL,
-  manufacturer varchar(250) NOT NULL,
-  model_name varchar(250) NOT NULL,
-  serial_number varchar(250) NOT NULL,
-  weight varchar(250) NOT NULL,
-  price DECIMAL(20,3) NOT NULL,,
-  PRIMARY KEY (car_id)
+    id SERIAL PRIMARY KEY,
+    manufacture_id INTEGER NOT NULL,
+    model VARCHAR NOT NULL,
+    serial VARCHAR NOT NULL,
+    weight INTEGER NOT NULL,
+    price INTEGER NOT NULL,
+    CONSTRAINT fk_manufacture
+      FOREIGN KEY(manufacture_id)
+	  REFERENCES manufacture(id)
 );
 
 -- Creation of transaction table
 CREATE TABLE IF NOT EXISTS transaction (
-  transaction_id INT NOT NULL,
-  customer_name varchar(250) NOT NULL,
-  customer_phone varchar(250) NOT NULL,
-  salesperson_name varchar(250) NOT NULL,
-  datetime_sale TIMESTAMP,
-  PRIMARY KEY (transaction_id),
-  CONSTRAINT fk_car_id
-      FOREIGN KEY(car_id) 
-	  REFERENCES car(car_id)
+    id SERIAL PRIMARY KEY,
+    c_name VARCHAR NOT NULL,
+    c_phone VARCHAR NOT NULL,
+    salesPerson_id INTEGER NOT NULL,
+    car_id INTEGER NOT NULL,
+    date TIMESTAMP NOT NULL,
+    CONSTRAINT fk_salesPerson
+      FOREIGN KEY(salesPerson_id)
+      REFERENCES salesPerson(id),
+    CONSTRAINT fk_car
+      FOREIGN KEY (car_id)
+      REFERENCES car(id)
 );
 
 
